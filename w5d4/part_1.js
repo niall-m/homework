80974083d7d6d9d638d06c843eace3851a4915a7
Niall Mahford <niall.mahford@gmail.com>
2017-11-02 08:06:24 -0700

    W5D4

---
+++ w5d4/part_1.js
@@ -0,0 +1,57 @@
+// madLib
+
+function madLib(verb, adj, noun) {
+  return `We shall ${verb.toUpperCase()} the ${adj.toUpperCase()} ${noun.toUpperCase()}.`
+}
+
+// isSubstring
+
+function isSubstring(phrase, subphrase) {
+  return phrase.includes(subphrase);
+}
+
+// fizzBuzz
+
+function fizzBuzz (array) {
+  const fizzBuzzArr = [];
+
+  array.forEach(el => {
+    if ((el % 3 === 0) ^ (el % 5 === 0)) {
+      fizzBuzzArr.push(el);
+    }
+  });
+
+  return fizzBuzzArr;
+}
+
+// isPrime
+
+function isPrime (n) {
+  if (n < 2) { return false; }
+
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+// sumOfNPrimes
+
+function sumOfNPrimes (n) {
+  let sum = 0;
+  let countPrimes = 0;
+  let i = 2;
+
+  while (countPrimes < n) {
+    if (isPrime(i)) {
+      sum += i;
+      countPrimes++;
+    }
+    i++;
+  }
+
+  return sum;
+}
